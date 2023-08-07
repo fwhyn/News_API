1922eb0074f3fb9ac4936351d9716718e5cb619b
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-08-07 09:11:18 +0700

    Date Time Format Bug Fix

--- app/src/main/java/com/fwhyn/noos/data/helper/Utils.kt
+++ app/src/main/java/com/fwhyn/noos/data/helper/Utils.kt
@@ -1,15 +1,15 @@
 package com.fwhyn.noos.data.helper
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.Random
+import java.util.*
 
 object Utils {
-    var vibrantLightColorList = arrayOf(
+    private var colorList = arrayOf(
         ColorDrawable(Color.parseColor("#ffeead")),
         ColorDrawable(Color.parseColor("#93cfb3")),
         ColorDrawable(Color.parseColor("#fd7a7a")),
@@ -21,30 +21,16 @@ object Utils {
     )
     val randomDrawableColor: ColorDrawable
         get() {
-            val index = Random().nextInt(vibrantLightColorList.size)
-            return vibrantLightColorList[index]
+            val index = Random().nextInt(colorList.size)
+            return colorList[index]
         }
 
-    //    public static String DateToTimeFormat(String oldstringDate){
-    //        PrettyTime p = new PrettyTime(new Locale(getCountry()));
-    //        String isTime = null;
-    //        try {
-    //            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'",
-    //                    Locale.ENGLISH);
-    //            Date date = sdf.parse(oldstringDate);
-    //            isTime = p.format(date);
-    //        } catch (ParseException e) {
-    //            e.printStackTrace();
-    //        }
-    //
-    //        return isTime;
-    //    }
-
+    @SuppressLint("SimpleDateFormat")
     fun getNewDateFormat(oldDate: String): String {
         return try {
-            val oldDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale(country))
+            val oldDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
             val parsedOldDate = oldDateFormat.parse(oldDate)
-            val newDateFormat = SimpleDateFormat("E, d MMM yyyy", Locale(country))
+            val newDateFormat = SimpleDateFormat("E, d MMM yyyy")
 
             newDateFormat.format(parsedOldDate!!)
         } catch (e: Exception) {
@@ -75,6 +61,7 @@ fun <T : java.io.Serializable> Intent.getSerializable(key: String, clazz: Class<
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         this.getSerializableExtra(key, clazz)
     } else {
+        @Suppress("UNCHECKED_CAST", "DEPRECATION")
         this.getSerializableExtra(key) as? T
     }
 }
